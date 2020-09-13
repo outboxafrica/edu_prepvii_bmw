537749b0b5547c0f0db45da4f87bbe99b1e933ce
Phillip Mambo <pbiscolt@gmail.com>
2020-09-13 23:55:57 +0300

    ch(add:delete confit)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+const mongoose = require('mongoose');
 require("dotenv").config();
 
 //DataBase connection
-require("./server/config/connect");
-const app = require("./server/app");
 
+const app = require("./server/app");
+const { Mongoose } = require("mongoose");
+
+const dbURI = 'mongodb+srv://bmwuser:bmw123@edu.4pjs5.mongodb.net/bmw-db?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+.then(() => {
+  console.info("\nDatabase Connection Established!");
+})
+.catch((err) => {
+  console.log("\nDatabase Connection Failed!");
+  console.error("Error Details: ", err);
+  console.log("\n\nDatabase Connection Failed, Retrying . . .");
+});
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`Using environment: ${process.env.NODE_ENV}`);
+  // console.log(`Using environment: ${process.env.NODE_ENV}`);
   console.log(`Server successfully started and listening on port http://localhost:${port}`);
 });
--- src/server/config/envTypes.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  DEVELOPMENT: "development",
-  PRODUCTION: "production",
-  LOCAL: "local",
-  };
-  
